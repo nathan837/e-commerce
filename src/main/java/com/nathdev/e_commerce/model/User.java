efa8a918eb23b6193f4c62aeff348a442f68e398
nathan837 <nathan777sam@gmail.com>
2026-04-17 16:25:49 +0300

    refactor: migrate database configuration from MySQL to PostgreSQL and update User entity mapping

--- src/main/java/com/nathdev/e_commerce/model/User.java
+++ src/main/java/com/nathdev/e_commerce/model/User.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 import org.hibernate.annotations.NaturalId;
 
-import jakarta.persistence.OneToMany;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -15,10 +13,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinColumns;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,6 +28,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "app_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
